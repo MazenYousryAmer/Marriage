d20dfa33d2d7ce9d3d93ae98b7c445aaeba0b1fe
Mazen <vis@Mazens-MacBook-Pro-2.local>
2017-08-08 01:25:58 +0200

    Final

--- Marriage/LandingViewController.swift
+++ Marriage/LandingViewController.swift
@@ -30,6 +30,13 @@ class LandingViewController: ParentViewController {
     
     @IBOutlet var img: UIImageView!
     
+    var facebookFirstName = ""
+    var facebookMail = ""
+    var faceBookID = ""
+    var facebookgender = ""
+    var facebookLastName = ""
+    var facebookURL = ""
+    
     //MARK:- variables
 //    var timer : NSTimer!
     
@@ -42,7 +49,7 @@ class LandingViewController: ParentViewController {
 //        let url = NSBundle.mainBundle().URLForResource("Lady", withExtension: "gif")
 //        self.img.image = UIImage.animatedImageWithAnimatedGIFData(NSData.init(contentsOfURL: url!)!)
         
-        self.btnFacebook.addTarget(self, action: #selector(self.loginButtonClicked), for: UIControlEvents.touchUpInside)
+        self.btnFacebook.addTarget(self, action: #selector(self.facebookBtnClicked), for: UIControlEvents.touchUpInside)
 
     
     }
@@ -219,6 +226,60 @@ class LandingViewController: ParentViewController {
         
     }
     
+    @objc func facebookBtnClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+                
+                let connection = GraphRequestConnection()
+                connection.add(GraphRequest(graphPath: "/me" , parameters: ["fields": "name,email,first_name,last_name,picture.type(small),gender"])) { httpResponse, result in
+                    switch result {
+                    case .success(let response):
+                        print("Graph Request Succeeded: \(response)")
+                        self.facebookgender = response.dictionaryValue!["gender"] as! String
+                        self.facebookFirstName = response.dictionaryValue!["first_name"] as! String
+                        self.facebookLastName = response.dictionaryValue!["last_name"] as! String
+                        self.facebookMail = response.dictionaryValue!["email"] as! String
+//                        self.facebookURL = response.dictionaryValue!["last_name"] as! String
+                        self.faceBookID = response.dictionaryValue!["id"] as! String
+
+                        
+                        if Token == ""
+                        {
+                            self.getTokenBeforeFacebookLogin()
+                        }
+                        else
+                        {
+                            self.callFacebookLogin()
+
+                        }
+                    case .failed(let error):
+                        print("Graph Request Failed: \(error)")
+                    }
+                }
+                connection.start()
+            }
+        }
+        //        loginManager.logIn([.publicProfile], viewController: self, completion: { loginResult in
+        //
+        //            switch loginResult{
+        //                            case .Failed(let error):
+        //                                print(error)
+        //                            case .Cancelled:
+        //                                print("User cancelled login.")
+        //                            case .Success(let grantedPermissions, let declinedPermissions, let accessToken):
+        //                                print("Logged in!")
+        //                            }
+        //            }
+        //        )
+    }
+    
     //MARK:- service HandShake
     func CallHandShake()
     {
@@ -339,43 +400,138 @@ class LandingViewController: ParentViewController {
             self.present(alert, animated: true, completion: nil)
         }
     }
-    @objc func loginButtonClicked() {
-        let loginManager = LoginManager()
-        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
-            switch loginResult {
-            case .failed(let error):
-                print(error)
-            case .cancelled:
-                print("User cancelled login.")
-            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
-                print("Logged in!")
-                
-                let connection = GraphRequestConnection()
-                connection.add(GraphRequest(graphPath: "/me" , parameters: ["fields": "name,email,first_name,last_name,picture.type(small),gender"])) { httpResponse, result in
-                    switch result {
-                    case .success(let response):
-                        print("Graph Request Succeeded: \(response)")
-                    case .failed(let error):
-                        print("Graph Request Failed: \(error)")
+
+    //MARK:- service login with FB
+    func getTokenBeforeFacebookLogin()
+    {
+        showLoading()
+        MyJSONParser.object(withUrlString: "\(URL)?case=hs&p1=mazinger5596&p2=CaptenMaged5_0&p3=Mazinger_1169&p4=Denam0_9987&p5=Dokfleed_3320", responseDelegate: self, responseSelector: #selector(LandingViewController.TokenBeforeFacebookLoginCallBk(_:)))
+    }
+    
+    func TokenBeforeFacebookLoginCallBk(_ dict : [String : AnyObject])
+    {
+        if let errorStatus = dict["Msg"] as? String
+        {
+            if errorStatus == "0"
+            {
+                if let data = dict["Data"] as? [AnyObject]
+                {
+                    if let dataObject = data[0] as? [String : AnyObject]
+                    {
+                        Token = dataObject["si"] as! String
+                        callFacebookLogin()
                     }
                 }
-                connection.start()
+            }
+            else if errorStatus == "-1"
+            {
+                print("Unauthorized access")
+                let alert = UIAlertController(title: NSLocalizedString("denied", comment: ""), message: NSLocalizedString("unauthorizedAccess", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("cancel", comment: ""), style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            else
+            {
+                print("error while making handshake")
+                let alert = UIAlertController(title: "Error", message: NSLocalizedString("denied", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("cancel", comment: ""), style: UIAlertActionStyle.default, handler: { action in
+                    // call service again
+                    self.CallHandShake()
+                }))
+                self.present(alert, animated: true, completion: nil)
             }
         }
-        //        loginManager.logIn([.publicProfile], viewController: self, completion: { loginResult in
-        //
-        //            switch loginResult{
-        //                            case .Failed(let error):
-        //                                print(error)
-        //                            case .Cancelled:
-        //                                print("User cancelled login.")
-        //                            case .Success(let grantedPermissions, let declinedPermissions, let accessToken):
-        //                                print("Logged in!")
-        //                            }
-        //            }
-        //        )
+        else
+        {
+            print("no internet")
+            let alert = UIAlertController(title: NSLocalizedString("connectionError", comment: ""), message: NSLocalizedString("internetError", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("tryAgain", comment: ""), style: UIAlertActionStyle.default, handler: { action in
+                // call service again
+                self.CallHandShake()
+                
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
+    
+    func callFacebookLogin()
+    {
+        showLoading()
+        MyJSONParser.object(withUrlString: "\(URL)?case=fblr&em=\(self.facebookMail)&id=\(self.faceBookID)&lng=\(languageID)&fn=&si=\(Token)", responseDelegate: self, responseSelector: #selector(LandingViewController.callFacebookLoginCallBk(_:)))
+    }
+    
+    func callFacebookLoginCallBk(_ dict : [String : AnyObject])
+    {
+        print("ok")
+        hideLoading()
+        if let errorStatus = dict["Msg"] as? String
+        {
+            if errorStatus == "5"
+            {
+                if let data = dict["Data"] as? [AnyObject]
+                {
+                    if let dataObject = data[0] as? [String : AnyObject]
+                    {
+                        // store credentials to Gloabals
+                        userFName = dataObject["MFstName"] as! String
+                        userLName = dataObject["MLstName"] as! String
+                        userPass = dataObject["PWD"] as! String
+                        userID = dataObject["MemberID_PK"] as! String
+                        
+                        // push main vc
+                        //                        self.performSegueWithIdentifier("HomeViewController", sender: nil)
+                        
+                        // set homeNav and side menu as root to window
+                        let delegate = UIApplication.shared.delegate as? AppDelegate
+                        let homeNav = self.storyboard?.instantiateViewController(withIdentifier: "InspireNav")
+                        let sideVc = self.storyboard?.instantiateViewController(withIdentifier: "SideViewController")
+                        
+                        let containerVC = MFSideMenuContainerViewController.container(withCenter: homeNav, leftMenuViewController: sideVc, rightMenuViewController: nil)
+                        containerVC?.panMode = MFSideMenuPanModeNone;
+                        delegate?.window?.rootViewController = containerVC
+                        
+                    }
+                }
+                else
+                {
+                    // set homeNav and side menu as root to window
+                    let delegate = UIApplication.shared.delegate as? AppDelegate
+                    let homeNav = self.storyboard?.instantiateViewController(withIdentifier: "InspireNav")
+                    let sideVc = self.storyboard?.instantiateViewController(withIdentifier: "SideViewController")
+                    
+                    let containerVC = MFSideMenuContainerViewController.container(withCenter: homeNav, leftMenuViewController: sideVc, rightMenuViewController: nil)
+                    containerVC?.panMode = MFSideMenuPanModeNone;
+                    delegate?.window?.rootViewController = containerVC
+                }
+            }
+            else if errorStatus == "-2"
+            {
+                print("error while login")
+                let alert = UIAlertController(title: NSLocalizedString("error", comment: ""), message: NSLocalizedString("nameOrPassError", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("cancel", comment: ""), style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            else
+            {
+                print("error while login")
+                let alert = UIAlertController(title: NSLocalizedString("error", comment: ""), message: NSLocalizedString("nameOrPassError", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("cancel", comment: ""), style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        else
+        {
+            print("no internet")
+            let alert = UIAlertController(title: NSLocalizedString("connectionError", comment: ""), message: NSLocalizedString("internetError", comment: ""), preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("tryAgain", comment: ""), style: UIAlertActionStyle.default, handler: { action in
+                // call service again
+                self.CallLogin()
+                
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
 
+    }
 
     
     //MARK:- Navigation
